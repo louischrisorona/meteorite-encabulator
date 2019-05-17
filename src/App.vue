bff356ddc933eaf8f3b00bf3ffb511da8063786e
louischrisorona <louischrisorona@gmail.com>
2019-05-17 11:13:22 -0700

    Setting up the component in App

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MeteorResults/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MeteorResults from './components/MeteorResults'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MeteorResults
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
