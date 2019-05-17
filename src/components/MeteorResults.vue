faf7e64c2bbe9eae726493d7fb20e367e9959895
louischrisorona <louischrisorona@gmail.com>
2019-05-17 14:56:21 -0700

    Added core structure to the component

--- src/components/MeteorResults.vue
+++ src/components/MeteorResults.vue
@@ -1,4 +1,37 @@
 <template>
-	Hellow World!
+	<vk-grid>
+		<div>
+			<div v-for="meteorite in meteorites">
+				<div v-for="(value, name) in meteorite">{{name}}:{{value}}</div>
+			</div>
+		</div>
+	</vk-grid>
 </template>
 
+<script>
+
+export default {
+	components: {
+		//any custom components needed
+	},
+	watch: {
+		//properties and data that needs to be tracked
+	},
+	computed: {
+		//data that needs to be calculated
+	},
+	data () {
+		return {
+		//necessary data to render
+		}
+	},
+	methods: {
+		//any helper functions
+	}
+
+}
+</script>
+
+<style>
+	
+</style>
