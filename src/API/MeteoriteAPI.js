7fc06e330a4d1f03954b8b030e8d9733253fddad
louischrisorona <louischrisorona@gmail.com>
2019-05-17 17:29:54 -0700

    Added a standalone API call

--- src/API/MeteoriteAPI.js
+++ src/API/MeteoriteAPI.js
@@ -1,8 +1,29 @@
 export default {
-	getMeteors () {
-		return whatever.then(JSON.parse).then();
+
+	getMeteors (url) {
+		return new Promise((resolve, reject) => {
+			const request = new XMLHttpRequest()
+			request.open("GET", url)
+			request.onload = () => {
+				if (request.status == 200) {
+					resolve(request.responseText)
+				} 
+				else {
+					reject(Error(request.statusText))
+				}
+			}
+			request.onerror = () => reject(Error("Network error"))
+			request.send()
+		})
 	},
-	getMeteor (id) {
-		return (id == meteors.id) ? meteors.id : ''
+	useJSON (addressURI){
+		return getMeteors(addressURI).then(JSON.parse);
 	}
+
+	useJSON("https://data.nasa.gov/resource/gh4g-9sfh.json")
+		.then( response => {
+			console.log("Success!", response);
+		}).catch( error => {
+			console.error("Failed!", error);
+		})
 }
